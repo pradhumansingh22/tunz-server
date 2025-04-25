1c3e189aca7411fc3c2bec5550d946951e69242d
prahumansingh22 <pradhuman362@gmail.com>
2025-04-25 10:23:29 +0530

    added the add song feature

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { createClient } from "redis";
 import WebSocket, { WebSocketServer } from "ws";
-import http from "http";
 import express from "express";
 import cors from "cors";
+
 const app = express();
 app.use(
   cors({
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   })
-);const server = app.listen(8080, () => {
+);
+
+const server = app.listen(8080, () => {
   console.log("WebSocket server running on port 8080");
 });
 const wss = new WebSocketServer({ server });
@@ -31,17 +33,29 @@ const connectToRedis = async () => {
 
 connectToRedis();
 
-app.get("/room/:roomId/messages", async(req, res) => {
+app.get("/room/:roomId/messages", async (req, res) => {
   const { roomId } = req.params;
   if (!roomId) res.send("Could not find Room Id!!!");
   try {
     const rawMessages = await redisClient.lRange(`chat:${roomId}`, 0, -1);
     const messages = rawMessages.map((m) => JSON.parse(m));
-    res.json({messages});
+    res.json({ messages });
   } catch (error) {
     console.error("Could not fetch messages", error);
   }
-})
+});
+
+app.get("/room/:roomId/songs", async (req, res) => {
+  const { roomId } = req.params;
+  if (!roomId) res.send("Could not find Room Id!!!");
+  try {
+    const rawSongs = await redisClient.lRange(`songs:${roomId}`, 0, -1);
+    const songs = rawSongs.map((song) => JSON.parse(song));
+    res.json({ songs });
+  } catch (error) {
+    console.error("Could not fetch songs", error);
+  }
+});
 
 const rooms = new Map<string, Set<WebSocket>>();
 
@@ -50,7 +64,10 @@ wss.on("connection", (ws) => {
   ws.on("error", console.error);
   ws.on("message", async (message) => {
     const { type, roomId, messageData } = JSON.parse(message.toString());
-    console.log(roomId);
+    
+    console.log("this is the room Id:", roomId);
+    const clients = rooms.get(roomId) || new Set();
+
     switch (type) {
       case "join":
         if (!rooms.has(roomId)) {
@@ -62,18 +79,24 @@ wss.on("connection", (ws) => {
       case "chat":
         await redisClient.lPush(`chat:${roomId}`, JSON.stringify(messageData));
         await redisClient.lTrim(`chat:${roomId}`, 0, 99);
-        const clients = rooms.get(roomId) || new Set();
+        
         for (const client of clients) {
           if (client.readyState === WebSocket.OPEN) {
             client.send(
-              JSON.stringify({ type: "chat", messageData, text: "yoooo" })
+              JSON.stringify({ type: "chat", messageData})
             );
           }
         }
         break;
 
       case "addSong":
-        //Handle Adding a song
+        await redisClient.lPush(`songs:${roomId}`, JSON.stringify(messageData));
+        console.log("messageData:", messageData);
+        for (const client of clients) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ type: 'addSong', messageData }));
+          }
+        }
         break;
       case "likeSong":
         //Handle Upvoting the song
