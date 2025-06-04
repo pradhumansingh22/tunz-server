e7251a9c06dfbe3d9ed01ea7a9f2dda9bd5e98fd
prahumansingh22 <pradhuman362@gmail.com>
2025-06-04 20:06:13 +0530

    Room Delete Logic

--- src/index.ts
+++ src/index.ts
@@ -81,7 +81,14 @@ wss.on("connection", (ws) => {
         if (members) {
           clients.delete(ws);
         }
-        if (members?.size === 0) rooms.delete(roomId);
+        if (members?.size === 0) {
+          rooms.delete(roomId);
+          axios
+            .delete(`http://localhost:3000/api/room/?roomId=${roomId}`)
+            .then((res) => {
+              console.log("Room Deleted");
+            });
+        }
         break;
 
       case "chat":
