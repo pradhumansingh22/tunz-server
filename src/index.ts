e05989bafbdfa085bd27e41c851e4db496eecda7
prahumansingh22 <pradhuman362@gmail.com>
2025-06-04 21:40:20 +0530

    Bug fixes

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://tunz.vercel.app/",
-      "https://tunz-h2ggo6v0k-pradhumansinghs-projects.vercel.app/",
+      "https://tunz.vercel.app",
+      "https://tunz-h2ggo6v0k-pradhumansinghs-projects.vercel.app",
     ],
     methods: ["GET", "POST"],
   })
